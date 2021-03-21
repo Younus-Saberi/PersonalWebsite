cc2c2858c7b31efb4f7c57b7f76d7062901f0a18
Younus-Saberi <younussaberi@gmail.com>
2021-03-21 19:49:57 +0530

    Added images and projects Links

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Student", "Designer", "Freelancer"],
+        strings: ["Developer", "Student", "Designer", ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
